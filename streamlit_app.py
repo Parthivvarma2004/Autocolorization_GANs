ffb547ea647dd0aa25cf7d4a8959ee30fcd3f17a
Parthiv <parthiv.varma@mail.utoronto.ca>
2023-12-01 23:32:31 -0500

    Added L1 Check

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,6 +54,31 @@ def preprocess_image(image):
 
 rf_loaded.eval()
 
+def rgb2lab(rgb):
+    if len(rgb.shape)==4:
+        arr = []
+        for img in rgb:
+            img = cv2.cvtColor(img, cv2.COLOR_RGB2LAB)
+            arr.append(img)
+        arr = np.array(arr)
+    else:
+        arr = cv2.cvtColor(rgb, cv2.COLOR_LAB2RGB)
+    return arr
+
+def calculate_l1_loss(predicted_image, target_image):
+    # Convert both images to LAB color space
+    predicted_lab = rgb2lab(predicted_image)
+    target_lab = rgb2lab(target_image)
+    
+    # Extract AB channels
+    predicted_ab = predicted_lab[:, :, 1:]  # Extract AB channels from predicted image
+    target_ab = target_lab[:, :, 1:]  # Extract AB channels from target image
+    
+    # Calculate L1 loss between AB values
+    l1_loss = np.mean(np.abs(predicted_ab - target_ab))
+    
+    return l1_loss
+
 def autocolorize(image):
     preprocessed_img = preprocess_image(image)
     
@@ -90,7 +115,7 @@ def autocolorize(image):
 
 st.title('Black & White Image Autocolorization')
 
-uploaded_file = st.file_uploader("Choose a black & white image...", type=["jpg", "png", "jpeg"])
+uploaded_file = st.file_uploader("Choose a black & white image to colorize", type=["jpg", "png", "jpeg"])
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
@@ -100,3 +125,29 @@ if uploaded_file is not None:
         # Perform autocolorization
         colored_image = autocolorize(image)
         st.image(colored_image, caption='Autocolorized Image',  width=300)
+        
+        
+st.title('L1 Checker')
+
+uploaded_file = st.file_uploader("Choose a colored image. Our model will produce the black and white version of it and color it so you can see how accuracte it is to the real image.", type=["jpg", "png", "jpeg"])
+colored_image = Image.fromarray(np.zeros((5, 5)))  # Replace (5, 5) with the desired shape
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Original Image', width=300)
+
+    if st.button('Check L1 Score'):
+        # Perform autocolorization
+        colored_image = autocolorize(image)
+        st.image(colored_image, caption='Autocolorized Image', width=300)
+
+        # Calculate L1 loss with a newly uploaded image
+        uploaded_image = Image.open(uploaded_file)  # Load the newly uploaded image again
+        uploaded_image = np.array(uploaded_image)    # Convert to numpy array
+
+        # Resize both images to ensure they have the same dimensions
+        colored_image_resized = colored_image.resize(uploaded_image.shape[1::-1])
+
+        # Calculate L1 loss
+        l1_loss = calculate_l1_loss(np.array(colored_image_resized), uploaded_image)
+        st.write(f"L1 Loss: {l1_loss/(256*256)}")
